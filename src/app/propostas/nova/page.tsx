40ff29c37cc7454fdcde32dec419f87bf2b6f4df
MrIgortbr <igortbr0412@hotmail.com>
2026-05-20 20:07:11 -0300

    bug fix v1.0.1

--- src/app/propostas/nova/page.tsx
+++ src/app/propostas/nova/page.tsx
@@ -2,8 +2,19 @@ import Footer from "@/components/footer/page";
 import LayoutMaster from "@/components/layout/page";
 import NewProposalComp from "@/components/newProposal/page";
 import Topbar from "@/components/Topbar/page";
+import { Suspense } from "react";
 
 export default function NewProposal() {
-    // eslint-disable-next-line react/no-children-prop
-    return <LayoutMaster header={<Topbar />} children={<NewProposalComp></NewProposalComp>} footer={<Footer></Footer>}></LayoutMaster>;
+    return (
+        <LayoutMaster
+            header={<Topbar />}
+            // eslint-disable-next-line react/no-children-prop
+            children={
+                <Suspense>
+                    <NewProposalComp></NewProposalComp>
+                </Suspense>
+            }
+            footer={<Footer></Footer>}
+        ></LayoutMaster>
+    );
 }
