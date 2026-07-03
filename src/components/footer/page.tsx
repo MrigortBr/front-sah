eb905ef832d4ee6c7977d568525b055b034609ca
MrIgortbr <igortbr0412@hotmail.com>
2026-07-03 10:57:18 -0300

    Update Logos

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -15,15 +15,9 @@ export default function Footer() {
         <FooterContainer>
             <FooterLeft>© 2026 SAH - Sistema de Acompanhamento de Habilitações</FooterLeft>
             <FooterRight>
-                {/* <ImageWrapper>
-                    <Image src="/especialistas.png" fill alt="Agora tem especialistas" style={{ objectFit: "contain" }} />
-                </ImageWrapper>
                 <ImageWrapper>
-                    <Image src="/ministerio.png" fill alt="Ministerio da saude" style={{ objectFit: "contain" }} />
+                    <Image src="/ministeriologo.png" fill alt="Ministerio da saude" style={{ objectFit: "contain" }} />
                 </ImageWrapper>
-                <ImageWrapper>
-                    <Image src="/gov.jpeg" fill alt="Governo Federal" style={{ objectFit: "contain" }} />
-                </ImageWrapper> */}
             </FooterRight>
         </FooterContainer>
     );
